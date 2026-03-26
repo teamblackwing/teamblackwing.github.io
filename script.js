cf7abd9c4c4fe9384fcc91a6c2893504a8c42d9f
Dheeran K. C. <dheerandps2010@gmail.com>
2026-03-26 22:14:57 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-const b = document.getElementById('bn-v');
-const d = document.getElementById('b');
-
-b.addEventListener('click', () => {
-    d.classList.toggle('d');
-    b.textContent = d.classList.contains('d') ? 'LIGHT_VIBE' : 'DARK_VIBE';
-});
-
 function go(src) {
     const o = document.getElementById('v');
     const i = document.getElementById('z');
@@ -20,16 +12,21 @@ function off() {
 function pop() {
     const h = document.getElementById('heart-box');
     
-    // reset first
     h.classList.remove('on');
     
-    // force a tiny delay so the transition triggers
     setTimeout(() => {
         h.classList.add('on');
     }, 10);
 
-    // hide after 1 second
     setTimeout(() => {
         h.classList.remove('on');
     }, 1000);
 }
+
+const b = document.getElementById('bn-v');
+
+b.addEventListener('click', () => {
+    document.body.classList.toggle('d');
+    
+    b.textContent = document.body.classList.contains('d') ? 'Light Mode' : 'Dark Mode';
+});
